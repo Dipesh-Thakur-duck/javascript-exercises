6275b992f787470f98195c04c88b99b4c1dcc6fe
Dipesh <letsmaildipesh1@gmail.com>
2025-06-01 18:31:58 +0545

    Add code to reverse a string
    A function is used to reverse a string provided as
     argument

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const wordArray = str.split('');
+    const reversedArray = [];
+    while(wordArray.length > 0) {
+        reversedArray.push(wordArray.pop());
+    }
+    const reversedWord = reversedArray.join('');
+    return reversedWord;
 };
-
+console.log(reverseString('hello'));
 // Do not edit below this line
 module.exports = reverseString;
