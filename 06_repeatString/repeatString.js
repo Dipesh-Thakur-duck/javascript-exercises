d06287843ff38289f6135782fa099e119d6a919d
Dipesh <letsmaildipesh1@gmail.com>
2025-06-01 18:15:05 +0545

    Add code to repeat a given string a specified number of times
    Using a function named `repeatString`, a word is
    repeated a specified number of times.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    if(num < 0) return 'ERROR';
+    let result='';
+    for(let i=0; i<num; i++){
+        result+=str;
+    }
+    return result;
 };
-
+console.log(repeatString("abc", 3)); // Expected output: "abcabcabc"
 // Do not edit below this line
 module.exports = repeatString;
