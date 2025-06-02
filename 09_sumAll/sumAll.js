33e5d99fe2124d28ed1ed83919d910163f30f14d
Dipesh <letsmaildipesh1@gmail.com>
2025-06-02 17:33:29 +0545

    Add code to sum all the numbers between given range including the extremes
    Using for loop and Number.isInteger to create
    logic of the program

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
-
-};
-
+const sumAll = function(num1,num2) {
+    if(num1 < 0 || num2 < 0){
+        return 'ERROR';
+    }else if(Number.isInteger(num1)&& Number.isInteger(num2)){
+         let sum = 0;
+        if(num1>num2){
+            for(let i = num2; i <= num1; i++){
+                sum += i;
+            }
+        }else{
+            for(let i = num1; i <= num2; i++){
+                sum += i;
+            }
+        }
+        return sum;
+    }else{
+        return 'ERROR';
+    }
+}
+   
+console.log(sumAll(1,4));
+console.log(sumAll(5,1));
 // Do not edit below this line
 module.exports = sumAll;
