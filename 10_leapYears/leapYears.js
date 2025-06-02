840ed63f2aec277de89efb38d4715dd7eb2e3cda
Dipesh <letsmaildipesh1@gmail.com>
2025-06-02 17:50:48 +0545

    Add code to check if a year is leap year
    Using nested if-else blocks, i create the logic to
     check if a year is leap year

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,31 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if(Number.isInteger(year)){
+        if(year>0){
+            if(year % 4 === 0){
+                if(year % 100 === 0){
+                    if(year % 400 === 0){
+                        return true;
+                    }else{
+                        return false;
+                    }
+                }else{
+                    return true;
+                }
+            }else{
+                return false;
+            }
+        }else{
+            return 'ERROR';
+        }
+    }else{
+        return 'ERROR';
+    }
 };
+
+console.log(1445);
+console.log(1984);
+console.log(3000);
+console.log('hello');
 
 // Do not edit below this line
 module.exports = leapYears;
