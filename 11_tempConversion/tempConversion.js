1257d0d9d6878e4aa10daa76b07d54bf84a34a57
Dipesh <letsmaildipesh1@gmail.com>
2025-06-02 18:07:32 +0545

    Add code to convert between celsius and fahrenheit
    Using formula and toFixed function to get the
    temprature value with at most one decimal value

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(value) {
+  const celsius = (value - 32) * 5/9;
+  return Number(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(value) {
+  const fahrenheit = (value * 9/5) + 32;
+  return Number(fahrenheit.toFixed(1));
 };
 
+console.log(convertToCelsius(78));
+console.log(convertToFahrenheit(32));
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
