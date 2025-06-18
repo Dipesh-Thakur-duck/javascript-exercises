a2ce257d3db478b0c3afbef16081018490f7faa1
Dipesh <letsmaildipesh1@gmail.com>
2025-06-18 16:38:12 +0545

    Add code to for arithmetic calculations
    Writing various functions to do arithmetic like
    add, subtract, multiply and power

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  arr.forEach((item)=>{
+    sum += item;
+  })
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let multiplication = 1;
+  arr.forEach((item)=>{
+    multiplication *= item;
+  })
+  return multiplication;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	if(exponent === 1){
+    return base;
+  }
+  return base * power(base, exponent-1);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if(number === 0){
+    return 1;
+  }
+	return number * factorial(number - 1)
 };
 
 // Do not edit below this line
