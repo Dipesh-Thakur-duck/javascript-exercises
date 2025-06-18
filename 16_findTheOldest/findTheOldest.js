d5d204993b04736f7d0860aa9a31595e6518e50d
Dipesh <letsmaildipesh1@gmail.com>
2025-06-18 18:57:59 +0545

    Add code to get details about the oldest person from array of objects with details
    Using map and find function on array to get the
    details of the oldest person

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,34 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const lifeSpansUser = arr.map((person)=>{
+        return {
+            person: person,
+            time: findLifeSpan(person)
+        }
+    });
+    const lifeSpans = lifeSpansUser.map(person => person.time);
+    const greatestLifeSpan = greatest(lifeSpans);
+    const oldestPerson = lifeSpansUser.find((users)=>users.time === greatestLifeSpan);
+    return oldestPerson.person;
 
 };
+
+function findLifeSpan(object){
+    const today = new Date();
+    const year = today.getFullYear();
+    if(!object.yearOfDeath){
+        return year - object.yearOfBirth;
+    }else return object.yearOfDeath - object.yearOfBirth;
+}
+
+function greatest(lifeSpans){
+    let max = lifeSpans[0];
+    for(let i = 1; i < lifeSpans.length; i++){
+        if(lifeSpans[i] > max){
+            max = lifeSpans[i];
+        }
+    }
+    return max;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
