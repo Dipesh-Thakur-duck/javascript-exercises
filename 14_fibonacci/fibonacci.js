ca27b8fc14d24369122f292b86dacefcc87f2717
Dipesh <letsmaildipesh1@gmail.com>
2025-06-18 17:41:28 +0545

    Add code to retrieve a fibonacci number based on position
    A function is created to return fibonacci number
    based on position input and allows strings as well
     if it can be converted into valid number

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+    if(typeof position === 'string'){
+        position = Number(position);
+    }
+    let a = 0;
+    let b = 1;
+    let c;
 
+    if(position === 0){
+        return a;
+    }else if(position === 1){
+        return b;
+    }else if(position > 1){
+        for(let i = 0; i <= position - 2; i++){
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }else if(position < 0){
+        return 'OOPS';
+    }
 };
 
 // Do not edit below this line
