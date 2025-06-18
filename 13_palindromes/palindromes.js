bf2abceed90a8a7ca278d6b374b47ffafd77c3d7
Dipesh <letsmaildipesh1@gmail.com>
2025-06-18 17:18:29 +0545

    Add code to check if a word or sentence is palindrome
    String is converted into array and various
    methods are applied to check is a word or
    sentence is palindrome

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const freshWord = word.toLowerCase().split('').filter(char=>/[a-z0-9]/.test(char)).join('');
+    const reverseWord = freshWord.split('').reverse().join('');
+    if(freshWord === reverseWord){
+        return true;
+    }else {
+        return false;
+    }
 };
 
 // Do not edit below this line
